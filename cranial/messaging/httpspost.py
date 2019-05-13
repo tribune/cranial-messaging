f911e5b322f51fbf0287fc10ed3a18937afa38fd
Matt Chapman <Matt@NinjitsuWeb.com>
2019-05-13 15:56:30 -0700

    Add https POST Notifier.

---
+++ cranial/messaging/httpspost.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+import requests
+
+from cranial.messaging import base
+
+
+class Notifier(base.Notifier):
+    @staticmethod
+    def send(address, message, endpoint, **kwargs):
+        count = 0
+        # Don't send empty messages.
+        while count < 3 and message.strip() != '':
+            response = requests.post('https://{}/{}'.format(
+                address, endpoint), data=message)
+            if response.status_code == requests.codes.ok:
+                try:
+                    return response.json()
+                except ValueError:
+                    return response.text
+            count += 1
+            sleep(5)
+
+        return False
