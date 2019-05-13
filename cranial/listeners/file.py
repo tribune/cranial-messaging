d1fa14fbbae07722a1badf059dfdbcda7cbfc958
Matt Chapman <Matt@NinjitsuWeb.com>
2019-05-13 15:58:01 -0700

    Add file listener.

---
+++ cranial/listeners/file.py
@@ -0,0 +1,17 @@
+from cranial.listeners import base
+from cranial.messaging.file import parts_to_path
+
+
+class Listener(base.Listener):
+    """Reads lines from a file.
+
+    Using the strange parameter names provides configuration magic.
+    @TODO Buffering?
+    """
+    def __init__(self, address=None, endpoint=None, path=None, **kwargs):
+        if not path:
+            path = parts_to_path(address, endpoint)
+        self.f = open(path, 'r')
+
+    def recv(self, **kwargs):
+        return next(self.f)
