f7578e284747d256ade056aa3a2b7fa4d1c1af60
Matt Chapman <Matt@NinjitsuWeb.com>
2019-05-13 17:53:41 -0700

    Add types & Yaml implementation.

--- cranial/servicediscovery/base.py
+++ cranial/servicediscovery/base.py
@@ -1,23 +1,42 @@
 from abc import ABCMeta, abstractmethod
+from typing import Any, Dict, List, Union  # noqa
+
+import yaml
+
+ServiceName = str
+ServiceValue = Union[str, List[str]]
+ServiceDefinition = Dict[str, ServiceValue]
+ServiceRegistry = Dict[ServiceName, ServiceDefinition]
 
 
 class Discovery(metaclass=ABCMeta):
-    def __init__(self, namespace):
+    def __init__(self, namespace: str) -> None:
         self.namespace = namespace
         self.prefix = self.namespace  # Backward compatibility.
-        self.services = {}
+        self.services = {}  # type: ServiceRegistry
         self.update()
 
     @abstractmethod
     def update(self):
         raise Exception('Not Implemented')
 
-    def get_metadata(self, service: str, key: str):
+    def get_metadata(self, service: str, key: str) -> ServiceValue:
         assert service in self.services
-        return self.services[service].get(key)
+        return self.services[service][key]
 
-    def get_instances(self, service: str):
-        return self.get_metadata(service, 'hosts')
+    def get_instances(self, service: str) -> List[str]:
+        return self.get_metadata(service, 'hosts')  # type: ignore
 
-    def get_protocol(self, service: str):
-        return self.get_metadata(service, 'protocol')
+    def get_protocol(self, service: str) -> str:
+        return self.get_metadata(service, 'protocol')  # type: ignore
+
+    def get_mode(self, service: str) -> str:
+        mode = self.get_metadata(service, 'mode')
+        assert mode in ['any', 'all']
+        return mode  # type: ignore
+
+
+class YamlDiscovery(Discovery):
+    def update(self):
+        with open(self.namespace) as f:
+            self.services = yaml.full_load(f)
