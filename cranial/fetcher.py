e786f86875aa118ae1a619e6545e287c1f50b680
Matt Chapman <Matt@NinjitsuWeb.com>
2019-06-10 22:57:23 -0700

    Clean-up old bad ideas.

--- cranial/fetcher.py
+++ cranial/fetcher.py
@@ -1,24 +1,24 @@
 """
-A Fetcher is a the combination of a Connector and a Parser, providing a
+A Fetcher is a the combination of a Connector and a SerDe, providing a
 standard way to retrieve data records as Dictionaries from arbitrary storage
 systems, via a generator.
 """
+from typing import Dict, Iterator
+
+import ujson
+
 from cranial.connectors.base import Connector
-from cranial.parsers import base, line
-
-
-class Parser():
-    pass
+from cranial.messaging.base import Serde
 
 
 class Fetcher():
-    def __init__(self, connector: Connector, parser: base.Parser=line.Parser) -> None:
+    def __init__(self, connector: Connector, serde: Serde = ujson) -> None:
         self.connector = connector
-        self.parser = parser
+        self.serde = serde
 
     def __iter__(self):
         return self.generator()
 
-    def generator(self):
-        for record in self.parser(self.connector):
-            yield record
+    def generator(self, target) -> Iterator[Dict]:
+        for record in self.connector.iterator(target):
+            yield self.serde.loads(record)
